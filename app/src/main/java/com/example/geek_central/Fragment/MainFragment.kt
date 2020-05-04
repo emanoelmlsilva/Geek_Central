a2ed0e94b46972ee3fbc86256e94940843e05299
emanoelmedeiros <emanoelmlsilva83@gmail.com>
2020-05-04 13:59:39 -0300

    adicionar popupmenu

--- app/src/main/java/com/example/geek_central/Fragment/MainFragment.kt
+++ app/src/main/java/com/example/geek_central/Fragment/MainFragment.kt
@@ -1,10 +1,14 @@
 package com.example.geek_central.Fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.example.geek_central.R
 import com.example.geek_central.databinding.FragmentMainBinding
 
 /**
@@ -12,14 +16,47 @@ import com.example.geek_central.databinding.FragmentMainBinding
  */
 class MainFragment : Fragment() {
 
+    private lateinit var bindBing: FragmentMainBinding;
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
 
-        val bindBing = FragmentMainBinding.inflate(layoutInflater, container, false)
+         bindBing = FragmentMainBinding.inflate(layoutInflater, container, false)
+
+        setClicledViews()
+
         return bindBing.root
     }
+
+    val clickListener = View.OnClickListener {view ->
+
+        showPopup(view)
+    }
+
+    private fun showPopup(view: View){
+        var popup: PopupMenu? = PopupMenu(context, view)
+        popup!!.inflate(R.menu.header_menu)
+
+        popup.setOnMenuItemClickListener (PopupMenu.OnMenuItemClickListener { item: MenuItem? ->
+            when (item!!.itemId){
+                R.id.Favorites -> Toast.makeText(context, "Favorito", Toast.LENGTH_SHORT).show();
+                R.id.Name -> Toast.makeText(context, "Nome", Toast.LENGTH_SHORT).show();
+                R.id.Total -> Toast.makeText(context, "Tota", Toast.LENGTH_SHORT).show();
+                R.id.Note -> Toast.makeText(context, "Nota", Toast.LENGTH_SHORT).show();
+            }
+
+            true
+        } )
+
+        popup.show()
+    }
+
+    private fun setClicledViews(){
+        bindBing.btnPopMenu.setOnClickListener(clickListener)
+    }
+
 
 }
