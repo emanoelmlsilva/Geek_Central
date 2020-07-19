c0fc3db0f066fa446db3a70c91222d9d593bfe0c
emanoelmedeiros <emanoelmlsilva83@gmail.com>
2020-07-19 01:25:42 -0300

    adicionar metodo para mudar estatus do bottom sheet

--- app/src/main/java/com/example/geek_central/viewmodels/BottomSheetLiveData.kt
+++ app/src/main/java/com/example/geek_central/viewmodels/BottomSheetLiveData.kt
@@ -1,22 +1,28 @@
 package com.example.geek_central.viewmodels
 
+import android.app.Activity
 import android.content.Context
+import android.content.res.Resources
+import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.TextView
+import android.view.ViewGroup
 import androidx.annotation.MainThread
 import androidx.lifecycle.LiveData
 import com.example.geek_central.R
-import com.example.geek_central.component.CounterComponent
 import com.example.geek_central.component.MenuEditMinComponent
 import com.example.geek_central.model.WorkGeek
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
+
 
 class BottomSheetLiveData(val context: Context? = null, var objGeek: WorkGeek? = null,var type: String = "min") : LiveData<Int>(),View.OnClickListener {
 
     private var bottomSheetDialog: BottomSheetDialog = BottomSheetDialog(context!!)
 
     private var myView : View
+
+    private lateinit var menuEditMin : MenuEditMinComponent
 
     private val listener = {v: View -> showDialog()}
 
@@ -30,19 +36,73 @@ class BottomSheetLiveData(val context: Context? = null, var objGeek: WorkGeek? =
 
         myView = view
 
+        setBottomSheetScreenAll()
+
+        statusBottomSheet()
+
+        if(objGeek != null) initComponets()
+
+    }
+
+    private fun initComponets(){
+        menuEditMin = MenuEditMinComponent(myView, this!!.objGeek!!)
+    }
+
+    private fun setBottomSheetScreenAll() {
+
+        val bottomSheetBehavior  = bottomSheetDialog.behavior
+
+        val bottomSheetPeekHeight = context!!.resources.getDimensionPixelSize(R.dimen.dimension_behavior)
+
+        bottomSheetBehavior.setPeekHeight(bottomSheetPeekHeight)
+
+        val childLayoutParams : ViewGroup.LayoutParams = myView.layoutParams
+
+        val displayMetrics : DisplayMetrics = DisplayMetrics()
+
+        Resources.getSystem().displayMetrics
+
+        (context as Activity).windowManager
+            .defaultDisplay
+            .getMetrics(displayMetrics)
+
+        childLayoutParams.height = displayMetrics.heightPixels - 300
+
+        myView.layoutParams = childLayoutParams
+    }
+
+    fun statusBottomSheet(){
+
+        bottomSheetDialog.behavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+
+            }
+
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                when (newState) {
+                    BottomSheetBehavior.STATE_COLLAPSED -> {
+                        menuEditMin.setVisible(View.VISIBLE)
+                    }
+                    BottomSheetBehavior.STATE_EXPANDED -> {
+                        menuEditMin.setVisible(View.GONE)
+                    }
+                }
+            }
+        })
     }
 
     fun setTypeMenu(myType: String = "min"){
         if(myType.equals("min")){
             MenuEditMinComponent(myView, this!!.objGeek!!)
-            type = myType
-        }else{
+
+        }else if(myType.equals("all")){
             //MenuEditAllComponent()
         }
     }
 
     fun setObjetWorkGeek(objt: WorkGeek){
         objGeek = objt
+        initComponets()
     }
 
     fun showDialog(){
