cc07dac2e22210d28f8db6c93a445e8b01d3de50
emanoelmedeiros <emanoelmlsilva83@gmail.com>
2020-07-26 20:29:18 -0300

    criar metodo onclick para componentes

--- app/src/main/java/com/example/geek_central/component/MenuEditComponent.kt
+++ app/src/main/java/com/example/geek_central/component/MenuEditComponent.kt
@@ -2,6 +2,8 @@ package com.example.geek_central.component
 
 import android.view.View
 import android.view.animation.TranslateAnimation
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import com.example.geek_central.R
@@ -11,27 +13,27 @@ import com.google.android.material.button.MaterialButton
 import com.google.android.material.textfield.TextInputLayout
 
 
-class MenuEditComponent(val view : View, val objGeek: WorkGeek? = null) {
+class MenuEditComponent(val view: View, val objGeek: WorkGeek? = null) {
 
     lateinit var title: TextView
 
-    private lateinit var componentCounteLeft : CounterComponent
+    private lateinit var componentCounteLeft: CounterComponent
 
-    private lateinit var componentCounteRigth : CounterComponent
+    private lateinit var componentCounteRigth: CounterComponent
 
-    private lateinit var inputName : TextInputLayout
+    private lateinit var inputName: TextInputLayout
 
-    private lateinit var note : NoteComponent
+    private lateinit var note: NoteComponent
 
-    private lateinit var categories : CategoriesComponent
+    private lateinit var categories: CategoriesComponent
 
-    private lateinit var inputAuthor : TextInputLayout
+    private lateinit var inputAuthor: TextInputLayout
 
-    private lateinit var inputSite : TextInputLayout
+    private lateinit var inputSite: TextInputLayout
 
-    private lateinit var btnSave : MaterialButton
+    private lateinit var btnSave: MaterialButton
 
-    private var status : Float = 0.0f
+    private var status: Float = 0.0f
 
 
     init {
@@ -44,9 +46,12 @@ class MenuEditComponent(val view : View, val objGeek: WorkGeek? = null) {
         loadingObject()
 
         visibiliteEditMinExtend()
+
+        onClick()
+
     }
 
-    private fun initView(){
+    private fun initView() {
 
         title = view.findViewById(R.id.txtTitle)
 
@@ -70,10 +75,10 @@ class MenuEditComponent(val view : View, val objGeek: WorkGeek? = null) {
 
     }
 
-    private fun visibiliteEditMinExtend(){
+    private fun visibiliteEditMinExtend() {
 
         //transation slide up/down view title
-        title.visibility = if(status < 0.0006F) View.VISIBLE else View.GONE
+        title.visibility = if (status < 0.0006F) View.VISIBLE else View.GONE
 
         //set visibility components
         setVisibilite(note.getCard())
@@ -90,7 +95,7 @@ class MenuEditComponent(val view : View, val objGeek: WorkGeek? = null) {
 
         slideUp(title)
 
-        if(status == 0.0f) slideUp(inputName, toX = view.width.toFloat()) else slideUp(inputName)
+        if (status == 0.0f) slideUp(inputName, toX = view.width.toFloat()) else slideUp(inputName)
 
         slideUp(note.getCard())
 
@@ -106,21 +111,27 @@ class MenuEditComponent(val view : View, val objGeek: WorkGeek? = null) {
     }
 
 
-    private fun setVisibilite(myView : View){
+    private fun setVisibilite(myView: View) {
 
-        if(status == 0.0F) {
+        if (status == 0.0F) {
             myView.visibility = View.GONE
-        }else {
+        } else {
             myView.visibility = View.VISIBLE
         }
     }
 
-    fun setStatus(statusUpdate : Float) {
+    fun setStatus(statusUpdate: Float) {
         status = statusUpdate
         visibiliteEditMinExtend()
     }
 
-    fun slideUp(myView: View, fromX : Float = myView.width.toFloat() - (status * 1000), toX : Float =  0F , fromY : Float = 0F, toY : Float = 0F) {
+    fun slideUp(
+        myView: View,
+        fromX: Float = myView.width.toFloat() - (status * 1000),
+        toX: Float = 0F,
+        fromY: Float = 0F,
+        toY: Float = 0F
+    ) {
 
         val animate = TranslateAnimation(
             fromX,  // fromXDelta
@@ -134,41 +145,69 @@ class MenuEditComponent(val view : View, val objGeek: WorkGeek? = null) {
         myView.startAnimation(animate)
     }
 
-    fun clickButtons(componentDefault : CounterComponent){
+    fun clickButtons(componentDefault: CounterComponent) {
 
-        componentDefault.getBtnAdd().setOnClickListener{
+        componentDefault.getBtnAdd().setOnClickListener {
             componentDefault.valueMoreLess(TypeMethodMath.SUM.toString())
         }
 
-        componentDefault.getBtnDel().setOnClickListener{
+        componentDefault.getBtnDel().setOnClickListener {
             componentDefault.valueMoreLess(TypeMethodMath.SUB.toString())
         }
 
-        componentDefault.getInputLayout().setOnClickListener{
+        componentDefault.getInputLayout().setOnClickListener {
             componentDefault.valueMoreLess(TypeMethodMath.SET.toString())
         }
 
     }
 
-    fun loadingObject(){
+    fun loadingObject() {
 
-        objGeek?.title?.let { title.text = it}
+        objGeek?.title?.let { title.text = it }
         objGeek?.title?.let { inputName.editText?.setText(it) }
         objGeek?.currentGeek?.let { componentCounteLeft.setTextLayout(it) }
         objGeek?.totalGeek?.let { componentCounteRigth.setTextLayout(it) }
-        objGeek?.popular?.note?.let { setValueNote(it.toInt())}
+        objGeek?.popular?.note?.let { setValueNote(it.toInt()) }
         objGeek?.author?.name?.let { inputAuthor.editText?.setText(it) }
-        objGeek?.hosted?.site?.let { inputSite.editText?.setText(it)}
+        objGeek?.hosted?.site?.let { inputSite.editText?.setText(it) }
 
         setIconFavorite()
     }
 
-    private fun setIconFavorite(){
-        if(objGeek?.popular?.favorite!!) inputName.setStartIconTintList(ContextCompat.getColorStateList(view.context,R.color.iconHeartEnable))
+    private fun setIconFavorite() {
+        if (objGeek?.popular?.favorite!!) inputName.setStartIconTintList(
+            ContextCompat.getColorStateList(
+                view.context,
+                R.color.iconHeartEnable
+            )
+        )
     }
 
-    private fun setValueNote(value : Int){
+    private fun setValueNote(value: Int) {
         note.getSeekBar().progress = value
         note.setValueComponentTextNote(value)
+    }
+
+    private fun onClick() {
+        note.getSeekBar().setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            override fun onProgressChanged(
+                seekBar: SeekBar,
+                progress: Int,
+                fromUser: Boolean
+            ) {
+
+                val progressHalf: Float = progress * 0.5F
+                note.getTxtNote().text = progressHalf.toString()
+
+            }
+        })
     }
 }
