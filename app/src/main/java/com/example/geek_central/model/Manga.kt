2ac4503245bec5d861a36cf76c752f09a698bdea
emanoelmedeiros <emanoelmlsilva83@gmail.com>
2020-07-26 16:53:44 -0300

    remover metodos e adicionar objetos author, popular e hoste

--- app/src/main/java/com/example/geek_central/model/Manga.kt
+++ app/src/main/java/com/example/geek_central/model/Manga.kt
@@ -4,8 +4,6 @@ class Manga(
     override var title: String? = null,
     override var currentGeek: Int? = null,
     override var totalGeek: Int? = null,
-    override var note: Double? = null,
-    override var favorite: Boolean? = null,
     override var categories: MutableList<String>? = null,
     override var author: Author? = null,
     override var popular: Popular? = null,
