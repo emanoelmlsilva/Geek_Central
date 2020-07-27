e9b6a1b5af69c2401cea696bd8943681a156af7b
emanoelmedeiros <emanoelmlsilva83@gmail.com>
2020-07-27 10:23:23 -0300

    adicionar tela de splash

---
+++ app/src/main/java/com/example/geek_central/SplashActivity.kt
@@ -0,0 +1,29 @@
+package com.example.geek_central
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+
+    private val splashTime = 3000L
+    private lateinit var myHandler : Handler
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        myHandler = Handler()
+
+        myHandler.postDelayed({
+            goToMainActivity()
+        }, splashTime)
+    }
+
+    private fun goToMainActivity(){
+        val mainActivity = Intent(applicationContext, MainActivity::class.java)
+        startActivity(mainActivity)
+        finish()
+    }
+}
