62d8e645db388b7444f9568c0ce69b08992848c7
emanoelmedeiros <emanoelmlsilva83@gmail.com>
2020-07-28 11:44:59 -0300

    criar interface observer

---
+++ app/src/main/java/com/example/geek_central/observer/IObservable.kt
@@ -0,0 +1,11 @@
+package com.example.geek_central.observer
+
+interface IObservable {
+
+    fun add(observer : IObserver, type : String)
+
+    fun remove(observer: IObserver)
+
+    fun sendUpdate()
+
+}
---
+++ app/src/main/java/com/example/geek_central/observer/IObserver.kt
@@ -0,0 +1,6 @@
+package com.example.geek_central.observer
+
+interface IObserver {
+
+    fun update(filter : String)
+}
