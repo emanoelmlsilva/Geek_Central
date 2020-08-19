7cec7b0e335ac1c48c1ccafe421052ed34ca9743
emanoelmedeiros <emanoelmlsilva83@gmail.com>
2020-08-19 19:54:47 -0300

    criar classe de controle do componente seasen

---
+++ app/src/main/java/com/example/geek_central/component/SeasonComponent.kt
@@ -0,0 +1,40 @@
+package com.example.geek_central.component
+
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import com.example.geek_central.R
+
+class SeasonComponent (val view : View){
+
+    private lateinit var spinner : Spinner
+    private lateinit var listItem : MutableList<String>
+
+    init{
+        initView()
+    }
+
+    private fun initView(){
+        spinner = view.findViewById(R.id.spinnerSeason)
+        listItem = mutableListOf("default")
+        setList()
+        ArrayAdapter(view.context, android.R.layout.simple_spinner_item, listItem)
+            .also { adapter -> adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                spinner.adapter = adapter
+            }
+    }
+
+    private fun mountItemList(count : Int){
+        for(item in 1..count){
+            listItem.add(item, item.toString())
+        }
+    }
+
+    fun getSpinner() : Spinner = spinner
+
+    fun setList(newCountItem : Int = 5){
+        mountItemList(newCountItem)
+    }
+
+    fun getCard() : View = view
+}
