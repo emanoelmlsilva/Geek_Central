fc24fc05e537ed1bbca06d87a24304f09b6d96c3
emanoelmedeiros <emanoelmlsilva83@gmail.com>
2020-08-19 22:35:32 -0300

    adicionar metodo para setar item na posição do spinner

--- app/src/main/java/com/example/geek_central/component/SeasonComponent.kt
+++ app/src/main/java/com/example/geek_central/component/SeasonComponent.kt
@@ -12,12 +12,18 @@ class SeasonComponent (val view : View){
 
     init{
         initView()
+
+        setItemSpinner()
     }
 
     private fun initView(){
+
         spinner = view.findViewById(R.id.spinnerSeason)
+
         listItem = mutableListOf("default")
+
         setList()
+
         ArrayAdapter(view.context, android.R.layout.simple_spinner_item, listItem)
             .also { adapter -> adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
                 spinner.adapter = adapter
@@ -31,6 +37,10 @@ class SeasonComponent (val view : View){
     }
 
     fun getSpinner() : Spinner = spinner
+
+    fun setItemSpinner(itemPostion : Int = 0){
+        spinner.setSelection(itemPostion)
+    }
 
     fun setList(newCountItem : Int = 5){
         mountItemList(newCountItem)
