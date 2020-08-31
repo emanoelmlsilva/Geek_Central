24e9856336dc85d68afb8b2e3f05aa3801b23afa
emanoelmedeiros <emanoelmlsilva83@gmail.com>
2020-08-31 16:11:44 -0300

    criar entidade para anime

---
+++ app/src/main/java/com/example/geek_central/model/WorkGeekAnime.kt
@@ -0,0 +1,21 @@
+package com.example.geek_central.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import org.jetbrains.annotations.NotNull
+
+@Entity(tableName = "work_geeks_animes")
+class WorkGeekAnime(@PrimaryKey(autoGenerate = true)
+                    @NotNull
+                    @ColumnInfo(name = "id_work_geek_anime") override var workGeekId : Long? =  null,
+                    @ColumnInfo(name = "title")
+                    override var title: String,
+                    @ColumnInfo(name = "current_geek")
+                    override var currentGeek: Int,
+
+                    @ColumnInfo(name = "season")
+                    override var season: Int? = null,
+                    @ColumnInfo(name = "total_geek")
+                    override var totalGeek: Int) : WorkGeek {
+}
