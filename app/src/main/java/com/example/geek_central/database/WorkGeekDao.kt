bf4259ff6cc1ba4342564d8a498d7f240faf1fb7
emanoelmedeiros <emanoelmlsilva83@gmail.com>
2020-09-01 18:44:39 -0300

    adicionar ordenação por titulo em lita de animes

--- app/src/main/java/com/example/geek_central/database/WorkGeekDao.kt
+++ app/src/main/java/com/example/geek_central/database/WorkGeekDao.kt
@@ -2,36 +2,39 @@ package com.example.geek_central.database
 
 import androidx.lifecycle.LiveData
 import androidx.room.*
-import com.example.geek_central.model.*
+import com.example.geek_central.model.WorkGeekAnime
+import com.example.geek_central.model.WorkGeekAnimeWithPopularAndHosted
+import com.example.geek_central.model.WorkGeekManga
+import com.example.geek_central.model.WorkGeekMangaWithPopularAndHosted
 
 @Dao
 interface WorkGeekDao {
 
     @Transaction
     @Query("SELECT * FROM work_geeks_mangas ORDER BY title ASC")
-    fun getWorkGeeksMangasOrderByTitle() : LiveData<List<WorkGeekMangaWithPopularAndHosted>>
+    fun getWorkGeeksMangasOrderByTitle(): LiveData<List<WorkGeekMangaWithPopularAndHosted>>
 
     @Transaction
     @Query("SELECT * FROM work_geeks_mangas INNER JOIN populares ON populares.workgeek_popular_id = work_geeks_mangas.id_work_geek_manga INNER JOIN hosts ON hosts.workgeek_hosted_id = work_geeks_mangas.id_work_geek_manga ORDER BY favorite DESC")
-    fun getWorkGeeksMangasOrdeByFavorite() : LiveData<List<WorkGeekMangaWithPopularAndHosted>>
+    fun getWorkGeeksMangasOrdeByFavorite(): LiveData<List<WorkGeekMangaWithPopularAndHosted>>
 
     @Transaction
     @Query("SELECT * FROM work_geeks_mangas INNER JOIN populares ON populares.workgeek_popular_id = work_geeks_mangas.id_work_geek_manga INNER JOIN hosts ON hosts.workgeek_hosted_id = work_geeks_mangas.id_work_geek_manga ORDER BY total_geek ASC")
-    fun getWorkGeeksMangasOrdeByTotal() : LiveData<List<WorkGeekMangaWithPopularAndHosted>>
+    fun getWorkGeeksMangasOrdeByTotal(): LiveData<List<WorkGeekMangaWithPopularAndHosted>>
 
     @Transaction
     @Query("SELECT * FROM work_geeks_mangas INNER JOIN populares ON populares.workgeek_popular_id = work_geeks_mangas.id_work_geek_manga INNER JOIN hosts ON hosts.workgeek_hosted_id = work_geeks_mangas.id_work_geek_manga ORDER BY grade DESC")
-    fun getWorkGeeksMangasOrdeByGrade() : LiveData<List<WorkGeekMangaWithPopularAndHosted>>
+    fun getWorkGeeksMangasOrdeByGrade(): LiveData<List<WorkGeekMangaWithPopularAndHosted>>
 
     @Transaction
-    @Query("SELECT * FROM work_geeks_animes")
-    fun getWorkGeeksAnimes() : LiveData<List<WorkGeekAnimeWithPopularAndHosted>>
-    
+    @Query("SELECT * FROM work_geeks_animes ORDER BY title ASC")
+    fun getWorkGeeksAnimes(): LiveData<List<WorkGeekAnimeWithPopularAndHosted>>
+
     @Query("SELECT id_work_geek_manga FROM work_geeks_mangas WHERE title = :title LIMIT 1")
-    fun findIdMangaByTitle(title : String) : Long
+    fun findIdMangaByTitle(title: String): Long
 
     @Query("SELECT id_work_geek_anime FROM work_geeks_animes WHERE title = :title LIMIT 1")
-    fun findIdAnimeByTitle(title: String) : Long
+    fun findIdAnimeByTitle(title: String): Long
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertManga(workGeek: WorkGeekManga)
@@ -46,8 +49,8 @@ interface WorkGeekDao {
     fun updateAnime(workGeek: WorkGeekAnime)
 
     @Query("DELETE FROM work_geeks_mangas WHERE id_work_geek_manga = :id")
-    fun deleteManga(id : Long)
+    fun deleteManga(id: Long)
 
     @Query("DELETE FROM work_geeks_animes WHERE id_work_geek_anime = :id")
-    fun deleteAnime(id : Long)
+    fun deleteAnime(id: Long)
 }
