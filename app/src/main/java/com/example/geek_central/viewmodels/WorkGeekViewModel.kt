2ad222aff61957d1c7ddcbcedd4d3954324e8dff
emanoelmedeiros <emanoelmlsilva83@gmail.com>
2020-09-03 16:39:50 -0300

    adicionar seleção de ordenão pelo tipo

--- app/src/main/java/com/example/geek_central/viewmodels/WorkGeekViewModel.kt
+++ app/src/main/java/com/example/geek_central/viewmodels/WorkGeekViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.viewModelScope
 import com.example.geek_central.database.WorkGeekDatabase
 import com.example.geek_central.database.WorkGeekRepository
+import com.example.geek_central.enums.TypeOrderBy
 import com.example.geek_central.model.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -32,11 +33,30 @@ class WorkGeekViewModel(application: Application) : AndroidViewModel(application
         return workRespository.findByTitleAnime(title)
     }
 
-    fun getAllWorkGeeksMangas(): LiveData<List<WorkGeekMangaWithPopularAndHosted>> =
-        workRespository.getAllWorkGeeksMangas()
+    fun getAllWorkGeeksMangas(typeOrder: String = TypeOrderBy.TITLE.toString()): LiveData<List<WorkGeekMangaWithPopularAndHosted>> {
+        return when(typeOrder){
+            TypeOrderBy.FAVORITE.toString() -> workRespository.getAllWorkGeeksMangasFavorites()
+            TypeOrderBy.TITLE.toString() -> workRespository.getAllWorkGeeksMangas()
+            TypeOrderBy.TOTAL.toString() -> workRespository.getAllWorkGeeksMangasTotal()
+            TypeOrderBy.GRADE.toString() -> workRespository.getAllWorkGeeksMangasGrades()
+            else ->  workRespository.getAllWorkGeeksMangas()
+        }
+    }
 
-    fun getAllWorkGeeksAnimes(): LiveData<List<WorkGeekAnimeWithPopularAndHosted>> =
-        workRespository.getAllWorkGeeksAnimes()
+
+
+    fun getAllWorkGeeksAnimes(typeOrder: String = TypeOrderBy.TITLE.toString()): LiveData<List<WorkGeekAnimeWithPopularAndHosted>> {
+
+        return when(typeOrder){
+            TypeOrderBy.FAVORITE.toString() -> workRespository.getAllWorkGeeksAnimesFavorites()
+            TypeOrderBy.TITLE.toString() -> workRespository.getAllWorkGeeksAnimes()
+            TypeOrderBy.TOTAL.toString() -> workRespository.getAllWorkGeeksAnimesTotal()
+            TypeOrderBy.GRADE.toString() -> workRespository.getAllWorkGeeksAnimesGrades()
+            else ->  workRespository.getAllWorkGeeksAnimes()
+        }
+
+    }
+
 
     fun insert(popular: Popular, host: Host, workGeekManga: WorkGeekManga) {
 
