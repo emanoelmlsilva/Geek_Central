ab961e5260cf299be3c9114a3afe528ece6e6af3
emanoelmedeiros <emanoelmlsilva83@gmail.com>
2020-09-14 20:25:15 -0300

    implementar vairavel categorias

--- app/src/main/java/com/example/geek_central/model/WorkGeekManga.kt
+++ app/src/main/java/com/example/geek_central/model/WorkGeekManga.kt
@@ -6,18 +6,22 @@ import androidx.room.PrimaryKey
 import org.jetbrains.annotations.NotNull
 
 @Entity(tableName = "work_geeks_mangas")
-data class WorkGeekManga(@PrimaryKey(autoGenerate = true)
-                         @NotNull
-                         @ColumnInfo(name = "id_work_geek_manga") override var workGeekId : Long? =  null,
-                         @ColumnInfo(name = "title")
-override var title: String = "",
-                         @ColumnInfo(name = "current_geek")
-override var currentGeek: Int = 0,
+data class WorkGeekManga(
+    @PrimaryKey(autoGenerate = true)
+    @NotNull
+    @ColumnInfo(name = "id_work_geek_manga") override var workGeekId: Long? = null,
+    @ColumnInfo(name = "title")
+    override var title: String = "",
+    @ColumnInfo(name = "current_geek")
+    override var currentGeek: Int = 0,
 
-                         @ColumnInfo(name = "season")
-override var season: String? = null,
-                         @ColumnInfo(name = "total_geek")
-override var totalGeek: Int = 0): WorkGeek {
+    @ColumnInfo(name = "season")
+    override var season: String? = null,
+    @ColumnInfo(name = "total_geek")
+    override var totalGeek: Int = 0,
+    @ColumnInfo(name = "categories")
+    override var categories: List<String> = ArrayList()
+) : WorkGeek {
 
 
 }
