7cb90df48d2b1937150251d3bf0e843f8768050d
emanoelmedeiros <emanoelmlsilva83@gmail.com>
2020-09-14 18:44:54 -0300

    criar metodo para adicionar ou remover item selecionado e para lista os item

--- app/src/main/java/com/example/geek_central/component/CategoriesComponent.kt
+++ app/src/main/java/com/example/geek_central/component/CategoriesComponent.kt
@@ -2,17 +2,35 @@ package com.example.geek_central.component
 
 import android.view.View
 import android.widget.LinearLayout
-import androidx.cardview.widget.CardView
 import com.example.geek_central.R
+import com.google.android.material.chip.Chip
 import com.google.android.material.chip.ChipGroup
 
-class CategoriesComponent(val view : View) {
+class CategoriesComponent(val view: View) {
 
-    private val groupCategories : ChipGroup = view.findViewById(R.id.groupCategories)
+    private val groupCategories: ChipGroup = view.findViewById(R.id.groupCategories)
 
-    private var linearLayout : LinearLayout = view.findViewById(R.id.categoriesComponent)
+    private var linearLayout: LinearLayout = view.findViewById(R.id.categoriesComponent)
 
-    fun getItemGroupCategories() : ChipGroup = groupCategories
+    private val listNames: MutableList<String> = mutableListOf()
 
-    fun getCard() : LinearLayout = linearLayout
+    fun getItemGroupCategories(): ChipGroup = groupCategories
+
+    fun getClickCips() {
+        for (index in 0 until groupCategories.childCount) {
+            val chipCurrent = groupCategories.getChildAt(index) as Chip
+            chipCurrent.setOnCheckedChangeListener { view, isChecked ->
+                if (isChecked) {
+                    listNames.add(view.text.toString())
+                } else {
+                    listNames.remove(view.text.toString())
+                }
+            }
+
+        }
+    }
+
+    fun getCard(): LinearLayout = linearLayout
+
+    fun getListItem() = listNames
 }
